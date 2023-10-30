cdc2cd8076d60b834d0f47a0a9612fb616bcaebe
sua <fiigjam0109@hansung.ac.kr>
2023-10-31 02:19:11 +0900

    toolbar

--- app/src/main/java/com/example/secondhandmarket/MainActivity.kt
+++ app/src/main/java/com/example/secondhandmarket/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.secondhandmarket
 
+import android.app.appsearch.AppSearchSchema.BooleanPropertyConfig
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
 import com.example.secondhandmarket.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -17,12 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     private val HomeFragment by lazy { HomeFragment() }
     private val ChatFragment by lazy { ChatFragment() }
-    private val MyPageFragment by lazy { MyPageFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        setContentView(R.layout.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
         val user = FirebaseAuth.getInstance().currentUser
         val uid = user?.uid
@@ -34,36 +39,28 @@ class MainActivity : AppCompatActivity() {
         val database = Firebase.database
         val myRef = database.getReference("message")
         myRef.setValue("Hello, Worlda!")
-
-        initNavigationBar()
     }
 
-    private fun initNavigationBar() {
-        val nav = findViewById<BottomNavigationView>(R.id.nav)
-        nav.run {
-            setOnNavigationItemSelectedListener {
-                when(it.itemId) {
-                    R.id.home -> {
-                        changeFragment(HomeFragment)
-                    }
-                    R.id.chat -> {
-                        changeFragment(ChatFragment)
-                    }
-                    R.id.myPage -> {
-                        changeFragment(MyPageFragment)
-                    }
-                }
-                true
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_chat -> {
+                Toast.makeText(applicationContext, "채팅 실행", Toast.LENGTH_LONG).show()
+                return true
             }
-            selectedItemId = R.id.home
+
+            R.id.menu_filter -> {
+                Toast.makeText(applicationContext, "검색조건 실행", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
-    }
 
-    private fun changeFragment(fragment: Fragment) {
+        fun changeFragment(fragment: Fragment) {
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.Frame, fragment)
-            .commit()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.Frame, fragment)
+                .commit()
+        }
     }
 }
