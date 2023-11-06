f20a353f66ceb75e221081e966977e7c48d836ef
jwlee <tlstprhl12@hansung.ac.kr>
2023-11-06 23:05:13 +0900

    클래스 생성 및 변경

--- app/src/main/java/com/example/secondhandmarket/WritePostActivity.kt
+++ app/src/main/java/com/example/secondhandmarket/WritePostActivity.kt
@@ -1,6 +1,5 @@
 package com.example.secondhandmarket
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,14 +7,10 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class WritePostActivity : AppCompatActivity() {
-    private lateinit var dbRef: DatabaseReference
-    private lateinit var storageRef: StorageReference
+    private lateinit var db: FirebaseFirestore
     private lateinit var titleEditText: EditText
     private lateinit var descriptionEditText: EditText
     private lateinit var priceEditText: EditText
@@ -25,9 +20,9 @@ class WritePostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.writepost)
 
-        // Firebase Database 및 Storage 초기화
-        dbRef = FirebaseDatabase.getInstance().getReference("Items")
-        storageRef = FirebaseStorage.getInstance().getReference()
+        // Firebase Firestore 초기화
+        db = FirebaseFirestore.getInstance()
+
         val mAuth = FirebaseAuth.getInstance()
         val currentUser: FirebaseUser? = mAuth.currentUser
 
@@ -49,10 +44,26 @@ class WritePostActivity : AppCompatActivity() {
 
     // 글 작성 및 업로드
     private fun uploadPost(title: String, description: String, price: String, seller: String?) {
-        val newItemRef: DatabaseReference = dbRef.push()
+        // Firestore 컬렉션 "items"를 참조
+        val itemsCollection = db.collection("items")
 
-        // 글 정보를 Firebase Realtime Database에 업로드
-        val item = ItemModel(title, description, true, price.toInt(), seller)
-        newItemRef.setValue(item)
+        // 새로운 문서를 생성하고 데이터를 설정
+        val itemData = hashMapOf(
+            "title" to title,
+            "description" to description,
+            "price" to price.toInt(),
+            "seller" to seller
+        )
+
+        // Firestore에 데이터 업로드
+        itemsCollection.add(itemData)
+            .addOnSuccessListener { documentReference ->
+                // 성공적으로 업로드된 경우
+                val documentId = documentReference.id
+                // 여기에서 추가적인 작업을 수행할 수 있음
+            }
+            .addOnFailureListener { e ->
+                // 업로드 실패 시 처리
+            }
     }
 }
